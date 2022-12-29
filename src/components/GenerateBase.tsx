9647511ca7ed105c778b46e2c47d7ccdcfb6bbe4
Helen <helenz.eltech@gmail.com>
2022-12-29 17:06:17 +0400

    get pixel data from image canvas

--- src/components/GenerateBase.tsx
+++ src/components/GenerateBase.tsx
@@ -1,24 +1,14 @@
 import React, {useState} from "react";
-import test from "../utilites/beacon.png";
 import {Bitmap} from "../utilites/library/bitmap";
 
 const GenerateBase = () => {
-    let [sizeImg, setSizeImg] = useState<{ width: number, height: number }>({width: 100, height: 100});
+   // let [sizeImg, setSizeImg] = useState<{ width: number, height: number }>({width: 100, height: 100});
     let [urlImg, setUrlImg] = useState<string | ArrayBuffer>('');
 
-    let bmp = new Bitmap(sizeImg.width, sizeImg.height)
+    //let bmp = new Bitmap(sizeImg.width, sizeImg.height);
 
-    // let createImg = () => {
-    //     for (let y = 0; y < sizeImg.height; y++) {
-    //         for (let x = 0; x < sizeImg.width; x++) {
-    //             if ((y + x) % 2 == 0) {
-    //                 bmp.pixel[x][y]=[1,0,0,1];
-    //             }
-    //
-    //         }
-    //     }
-    // }
-    // createImg()
+    let canvas = document.createElement("canvas");
+    let ctx = canvas.getContext('2d');
 
     let encodeImgFileAsUrl = (element: File) => {
         let reader = new FileReader();
@@ -30,12 +20,43 @@ const GenerateBase = () => {
         reader.readAsDataURL(element);
     }
 
+    let imgToCanvasAsBitmap = (element: File) => {
+        createImageBitmap(element).then(bitmap => {
+            canvas.width = bitmap.width;
+            canvas.height = bitmap.height;
+            if (ctx !== null) {
+                ctx.drawImage(bitmap, 0, 0);
+                console.log(ctx.getImageData(0, 0, bitmap.width, bitmap.height));
+            }
+            return bitmap
+        });
+    }
+
+    console.log(urlImg)
+
+    // let createImg = () => {
+    //     for (let y = 0; y < sizeImg.height; y++) {
+    //         for (let x = 0; x < sizeImg.width; x++) {
+    //             if ((y + x) % 2 == 0) {
+    //                 bmp.pixel[x][y] = [1, 0, 0, 1];
+    //
+    //             }
+    //         }
+    //     }
+    // }
+
+
+    // createImg()
+
     return (
         <>
             {/*<img src={`${bmp.dataURL()}`}/>*/}
-            <input type="file" onChange={(e)=>{
+            <input type="file" onChange={(e) => {
                 let fileList = e.target.files;
-                if (fileList != null && fileList[0] != null) encodeImgFileAsUrl( fileList[0]);
+                if (fileList != null && fileList[0] != null) {
+                    encodeImgFileAsUrl(fileList[0]);
+                    imgToCanvasAsBitmap(fileList[0]);
+                }
             }}/>
             <div style={{margin: "20px 10px"}}>
                 {urlImg && <img src={`${urlImg}`} width="400px" height="400px"/>}
